bcd7e3d86837d5bb0f30770df9b5c6056a459335
goisais <lefufuben@gmail.com>
2026-02-07 01:14:25 +0900

    デザイン変更2

--- taskplanner/forms.py
+++ taskplanner/forms.py
@@ -6,11 +6,13 @@ class ScheduleForm(forms.ModelForm):
     class Meta:
         model = Schedule
         fields = "__all__"
+        new_var = "class"
         widgets = {
-            "title": forms.TextInput(attrs={"class": "input-box"}),
-            "date": forms.DateTimeInput(attrs={"type": "datetime-local", "class": "input-box"}),
-            "priority": forms.Select(attrs={"class": "input-box"}),
-            "duration": forms.TextInput(attrs={"class": "input-box"}),
-            "memo": forms.Textarea(attrs={"class": "textarea-box"}),
-            "date": forms.DateTimeInput(attrs={"type": "datetime-local"}),
+            "title": forms.TextInput(attrs={new_var: "input-box"}),
+            "date": forms.DateTimeInput(
+                attrs={"type": "datetime-local", new_var: "input-box"}
+            ),
+            "priority": forms.Select(attrs={new_var: "input-box"}),
+            "duration": forms.TextInput(attrs={new_var: "input-box"}),
+            "memo": forms.Textarea(attrs={new_var: "textarea-box"}),
         }
